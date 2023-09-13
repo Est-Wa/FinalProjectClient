d9c6c7e79bfaca4bd9f4b069efabe721753c99be
EST-WA <ewand1043@gmail.com>
2023-09-13 15:31:29 +0300

    higlight current lesson

--- src/components/Authorized/LearningPlan/LessonCard.jsx
+++ src/components/Authorized/LearningPlan/LessonCard.jsx
@@ -1,10 +1,29 @@
-import { Card, Typography } from '@mui/material';
+// LessonCard.jsx
+
+import { Card, Typography, Button } from '@mui/material';
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 export default function LessonCard(props) {
-    const { title, content } = props;
-    return ( <Card sx={{width:'20%', backgroundColor:'#e6faff'}}>
+    const { title, content, isCurrentLesson } = props;
+    const navigate = useNavigate(); // Initialize navigate
+
+    const redirectToCurrentLesson = () => {
+        // Implement logic to determine the URL for the current lesson
+        const currentLessonURL = '/my-lesson'; // Replace with the actual URL
+        navigate(currentLessonURL); // Use navigate for redirection
+    };
+
+    return (
+        <Card sx={{ width: '20%', backgroundColor: isCurrentLesson ? '#e6faff' : '' }}>
             <Typography variant='h4'>{title}</Typography>
             <Typography variant='h5'>{content}</Typography>
-        </Card>)
+            {isCurrentLesson && (
+                <>
+                    <Typography variant='h6'>אתה נמצא כאן</Typography>
+                    <Button onClick={redirectToCurrentLesson}>למעבר לשיעור</Button>
+                </>
+            )}
+        </Card>
+    );
 }
--- src/components/Authorized/LearningPlan/index.jsx
+++ src/components/Authorized/LearningPlan/index.jsx
@@ -1,11 +1,32 @@
-import { Box } from "@mui/material";
-import React from "react";
-import LessonCard from "./LessonCard";
+// LearningPlan.jsx
 
-export default function LearningPlan(){
-    const elements = {'חלק א':'לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית ושבעגט ליבם סולגק. בראיט ולחת צורק מונחף, בגורמי מגמש. תרבנך וסתעד לכנו סתשם השמה - לתכי מורגם בורק? לתיג ישבעס.','חלק ב':'לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית ושבעגט ליבם סולגק. בראיט ולחת צורק מונחף, בגורמי מגמש. תרבנך וסתעד לכנו סתשם השמה - לתכי מורגם בורק? לתיג ישבעס.','חלק ג':'לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית ושבעגט ליבם סולגק. בראיט ולחת צורק מונחף, בגורמי מגמש. תרבנך וסתעד לכנו סתשם השמה - לתכי מורגם בורק? לתיג ישבעס.','חלק ד':'לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית ושבעגט ליבם סולגק. בראיט ולחת צורק מונחף, בגורמי מגמש. תרבנך וסתעד לכנו סתשם השמה - לתכי מורגם בורק? לתיג ישבעס.'}
-    return(
-    <Box sx={{display:'flex', justifyContent:'space-between',padding:'20px'}}>
-        {Object.keys(elements).map((e, i ) => <LessonCard key={i} title = {e} content = {elements[e]}></LessonCard>)}
-    </Box>)
+import { Box } from "@mui/material";
+import React, {useContext} from "react";
+import LessonCard from "./LessonCard";
+import { LessonContext } from "../../../context/lessonContext";
+
+export default function LearningPlan() {
+
+    const { stage } = useContext(LessonContext); // Access the LessonContext
+    const elements = {
+        'חלק א': '...', // Your content here
+        'חלק ב': '...', // Your content here
+        'חלק ג': '...', // Your content here
+        'חלק ד': '...', // Your content here
+    };
+
+    const currentLesson = stage - 1; // Replace with the actual current lesson
+
+    return (
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
+            {Object.keys(elements).map((e, i) => (
+                <LessonCard
+                    key={i}
+                    title={e}
+                    content={elements[e]}
+                    isCurrentLesson={i === currentLesson} // Check if it's the current lesson
+                />
+            ))}
+        </Box>
+    );
 }
